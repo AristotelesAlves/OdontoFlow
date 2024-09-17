739aebddac96d0b29794ddbd148e7f8fffd524dd
Aristoteles <aristoteles@localhost.localdomain>
2024-09-17 16:45:04 -0300

    header completo

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/dashboard">Return Home</Link>
+    <div className='bg-black w-full h-screen overflow-hidden flex justify-center items-center text-white flex-col gap-2'>
+      <h2>Página não encontrada</h2>
+      <p className='text-9xl font-bold'>404</p>
+      <Link className='p-2 bg-blue text-white text-xl hover:bg-blue hover:bg-opacity-70 rounded-xl' href="/dashboard">Voltar para Dashboard</Link>    
     </div>
   )
 }
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,36 +1,37 @@
 "use client"
+import { Bell, Gear, MagnifyingGlass, Tooth } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 
 export default function Header(){
 
     const pathname = usePathname();
-    const currentPage = pathname.split('/').pop()
+    const paginaAtual = pathname.split('/').pop()
 
     return (
-        <header className="flex w-full justify-between items-center pt-2">
+        <header className="flex w-full justify-between items-center pt-5">
             <div className="flex gap-4 items-center">
-                <figure>
-                    <img className="h-12 w-12 rounded-full bg-blue" src="" alt=""/>
+                <figure className="h-14 w-14 text-white flex items-center justify-center rounded-full bg-blue" >
+                    <Tooth size={32} weight="fill"/>
                 </figure>
                 <nav>
                     <ul className="flex gap-2 items-center">
-                        <li className={`py-2 px-4 rounded-3xl border-2 ${currentPage == 'dashboard' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
+                        <li className={`py-2 px-4 rounded-3xl border-2 hover:bg-blue hover:bg-opacity-65 hover:text-white ${paginaAtual == 'dashboard' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
                             <Link href="/dashboard">
                                 Dashboard
                             </Link>
                         </li>
-                        <li className={`py-2 px-4 rounded-3xl border-2 ${currentPage == 'historico' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
+                        <li className={`py-2 px-4 rounded-3xl border-2 hover:bg-blue hover:bg-opacity-65 hover:text-white ${paginaAtual == 'historico' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
                             <Link href="/historico">
                                 Histórico
                             </Link>
                         </li>
-                        <li className={`py-2 px-4 rounded-3xl border-2 ${currentPage == 'estoque' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
+                        <li className={`py-2 px-4 rounded-3xl border-2 hover:bg-blue hover:bg-opacity-65 hover:text-white ${paginaAtual == 'estoque' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
                             <Link href="/estoque">
                                 Estoque
                             </Link>
                         </li>
-                        <li className={`py-2 px-4 rounded-3xl border-2 ${currentPage == 'usuarios' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
+                        <li className={`py-2 px-4 rounded-3xl border-2 hover:bg-blue hover:bg-opacity-65 hover:text-white ${paginaAtual == 'usuarios' ? 'bg-blue text-white border-blue border-opacity-50' : null}`}>
                             <Link href="/usuarios">
                                 Usuário
                             </Link>
@@ -39,14 +40,32 @@ export default function Header(){
                 </nav>
             </div>
             <div className="flex gap-4 items-center">
-                <span>
-                    input
-                </span>
+                <div className="flex gap-1 items-center border rounded-3xl h-14 px-2">
+                    <MagnifyingGlass size={25}/>
+                    <input type="text" placeholder="Buscando por algo?" className=" border-none outline-none w-[200px]" name="" id="" />
+                </div>
+                
                 <nav>
                     <ul className="flex gap-2 items-center">
-                        <li>config</li>
-                        <li>bell</li>
-                        <li>user</li>
+                        <li className="w-14 h-14 flex items-center justify-center border rounded-full p-2 relative">
+                            <Gear size={25}/>
+                        </li>
+                        <li className="w-14 h-14 flex items-center justify-center border rounded-full p-2 relative">
+                            <Bell weight="fill" size={25}/>
+                            <div className="w-5 h-5 flex items-center justify-center text-xs font-semibold text-white absolute right-0 top-0 bg-red rounded-full">
+                                <span>
+                                    1
+                                </span>
+                            </div>
+                        </li>
+                        <li>
+                            <figure>
+                                <img className="w-14 h-14 rounded-full"
+                                  src="https://i.pinimg.com/736x/68/2d/d0/682dd04c8020d93412298636e510572d.jpg" 
+                                  alt="" 
+                                />
+                            </figure>
+                        </li>
                     </ul>
                 </nav>
             </div>
